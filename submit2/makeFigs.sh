be1085e640afd5a8bfb5f0d397027cba1bbfe192
Hilary Weller <h.weller@reading.ac.uk>
2020-04-06 22:34:22 +0100

    Putting figures into a single .ps file per figure

---
+++ submit2/makeFigs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+cd makeFigs
+
+for file in bubble bubble_singleUnderRes multiFluidBubble_500_1 multiFluidBubble_500_2 multiFluidBubble_1000; do
+    echo $file
+    lyx --export pdflatex -f $file
+    pdflatex $file
+    pdf2ps $file.pdf
+    makebb $file.ps
+    mv $file.ps $file.eps
+    pdfcrop $file.pdf $file.cropped.pdf
+    mv $file.cropped.pdf $file.pdf
+    mv $file.eps $file.pdf ../figures
+    gv ../figures/$file.eps &
+done
+
+cd ..
+
